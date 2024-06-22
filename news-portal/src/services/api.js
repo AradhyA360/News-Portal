076cef43ce74d022459bc5406980206d11ef0b12
AradhyA360 <aradhyaaa@hotmail.com>
2024-06-22 13:12:55 +0530

    UPDATED

--- news-portal/src/services/api.js
+++ news-portal/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_KEY = "a59e3bbf1bfa45b2a7cfe4144c795359";
+const VITE_API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = "https://newsapi.org/v2/top-headlines?country=in&apiKey";
 
 export const fetchArticles = async (category, page) => {
   try {
     const response = await axios.get(`${BASE_URL}/top-headlines`, {
       params: {
-        apiKey: API_KEY,
+        apiKey: VITE_API_KEY,
         category: category,
         page: page,
         pageSize: 12,
